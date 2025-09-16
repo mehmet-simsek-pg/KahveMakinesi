146264c4dad2e6d531ba686f7732a76fba23e0ba
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-09-16 20:36:33 +0200

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -2,6 +2,8 @@ package KahveMakinesi;
 
 public class Test1 {
     public static void main(String[] args) {
+
         System.out.println("Bu satir düzeltildi");
+        System.out.println("Bu testte update yapilmistir");
     }
 }
